41ebbd8d7dfbbb97068d381c0b185b1a3b90558d
Vavrykovych <vavrycovych@gmail.com>
2023-03-14 12:27:46 +0200

    Added functionality for role assignment

--- DotaDiscordBot/DotaDiscordBot/Modules/CommonCommands.cs
+++ DotaDiscordBot/DotaDiscordBot/Modules/CommonCommands.cs
@@ -12,7 +12,16 @@ namespace DotaBot.Modules
 {
     public class CommonCommands : ModuleBase<SocketCommandContext>
     {
+        private readonly CommandService _commandService;
+
+        public CommonCommands(CommandService commandService)
+        {
+            _commandService = commandService;
+        }
+
+
         [Command("ping")]
+        [Summary("Ping")]
         public async Task Ping()
         {
             await ReplyAsync(Context.User.Mention + " pong");
@@ -34,7 +43,48 @@ namespace DotaBot.Modules
             Random rand = new Random();
             var padarIndex = rand.Next(0, filteredUsers.Count());
             var pidarUser = filteredUsers[padarIndex];
+
             await ReplyAsync($"Підара знайдено, це - {pidarUser.DisplayName}");
         }
+
+        [Command("help")]
+        [Summary("Displays a list of available commands or information about a specific command.")]
+        public async Task HelpCommand([Remainder][Summary("The name of the command to get help for (optional).")] string commandName = null)
+        {
+            if (commandName == null)
+            {
+                // Display a list of available commands.
+                var commands = _commandService.Commands;
+
+                var embed = new EmbedBuilder()
+                    .WithTitle("Available commands:")
+                    .WithDescription(string.Join("\n", commands.Select(c => c.Name)))
+                    .WithColor(Color.Green)
+                    .Build();
+
+                await ReplyAsync(embed: embed);
+            }
+            else
+            {
+                // Display help information for a specific command.
+                var command = _commandService.Commands.FirstOrDefault(c => c.Name == commandName);
+
+                if (command == null)
+                {
+                    await ReplyAsync($"Command '{commandName}' not found.");
+                    return;
+                }
+
+                var embed = new EmbedBuilder()
+                    .WithTitle($"Help: {command.Name}")
+                    .WithDescription(command.Summary)
+                    .AddField("Usage", $"!{command.Name} {string.Join(" ", command.Parameters.Select(p => $"[{p.Name}]"))}")
+                    .WithColor(Color.Blue)
+                    .Build();
+
+                await ReplyAsync(embed: embed);
+            }
+        }
+
     }
 }
---
+++ DotaDiscordBot/DotaDiscordBot/Modules/RolesCommands.cs
@@ -0,0 +1,71 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotaBot.Modules
+{
+    public class RolesCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("add-role")]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        public async Task AssignRoleCommand(SocketGuildUser user, SocketRole role)
+        {
+            await AssignRole(user, role);
+        }
+
+        [Command("remove-role")]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        public async Task UnassignCommand(SocketGuildUser user, SocketRole role)
+        {
+            await UnassignRole(user, role);
+        }
+
+        public async Task AssignRole(SocketGuildUser user, SocketRole role)
+        {
+            try
+            {
+                if (user.Roles.Contains(role))
+                {
+                    await ReplyAsync($"{user.Mention} already has the {role.Name} role.");
+                    return;
+                }
+
+                await user.AddRoleAsync(role);
+                await ReplyAsync($"{user.Mention} has been assigned the {role.Name} role.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to assign role to {user.Username}: {ex.Message}");
+                await ReplyAsync($"Failed to assign role to {user.Mention}. Please try again later.");
+            }
+        }
+
+        public async Task UnassignRole(SocketGuildUser user, SocketRole role)
+        {
+            try
+            {
+                // Check if the user doesn't have the role.
+                if (!user.Roles.Contains(role))
+                {
+                    await ReplyAsync($"{user.Mention} doesn't have the {role.Name} role.");
+                    return;
+                }
+
+                // Remove the role from the user.
+                await user.RemoveRoleAsync(role);
+                await ReplyAsync($"{user.Mention} has been unassigned the {role.Name} role.");
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that might occur.
+                Console.WriteLine($"Failed to unassign role from {user.Username}: {ex.Message}");
+                await ReplyAsync($"Failed to unassign role from {user.Mention}. Please try again later.");
+            }
+        }
+    }
+}
